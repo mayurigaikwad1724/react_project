c952d4ee750a141e85e8c2d077bf58db054ca949
Mayuri Gaikwad <mayuri20@navgurukul.org>
2023-07-02 22:54:17 +0530

    half movie_row complete

---
+++ app/header.js
@@ -0,0 +1,29 @@
+const Header =() => {
+    return(
+       <>
+           <div className="row1"></div>
+                <div className="row2">
+                    <div className="col1">
+                        <img src="Icon - Logo.svg"/>
+                    </div>
+                    <div className="col2">
+                        <div className="word"> Movie Hub </div>
+                    </div>
+                    <div className="col3">
+                        <div className="word1"> Watch </div>
+                    </div>
+                    <div className="col4">
+                        <div className="word2">Add Movie</div>
+                    </div>
+                </div>
+                <div className="row3"></div>
+                <div className="row4">
+                    <div className="row4_col">
+                        <div className="word3">Watch</div>
+                    </div>
+                </div>
+                <div className="row5"></div>
+       </>
+    );
+}
+export default Header;
---
+++ app/movie_row.js
@@ -0,0 +1,14 @@
+const Movie_row =() => {
+    return(
+       <>
+            <div className="row6">
+                <div className="col5">
+                    <img src="image 1 .png"/>
+                </div>
+                <div className="col6"></div>
+            </div>
+            <hr color="white" width="75%"/>
+       </>
+    );
+}
+export default Movie_row;
